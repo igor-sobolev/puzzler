32e16c5daa897d62e5c84068febbcb41335a4aa6
Igor <lucifer-angelok@mail.ru>
2019-06-25 12:20:40 +0300

    [ADD]Project setup

---
+++ src/containers/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+import { Header } from '@/components/Header'
+
+export const App = () => {
+  return (
+    <div className="App">
+      <Header></Header>
+    </div>
+  );
+}
