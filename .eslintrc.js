2227b4ec94aa5ce25cb115f540f1f9b760defc2d
Igor <lucifer-angelok@mail.ru>
2019-06-25 17:58:45 +0300

    [CHA] Eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,21 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
+    es6: true
   },
-  extends: ['plugin:vue/essential', '@vue/standard'], // activate vue related rules
-  plugins: ['vue'],
+  plugins: ['react'],
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
   rules: {
-    'quotes': ['error', 'single'],
-    'code': ['warning', 100],
-    'semi': ['error', 'never'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'indent': [2, 2, { SwitchCase: 1}],
-    'vue/component-name-in-template-casing': ['error', 'PascalCase'],
-    'vue/script-indent': ['error', 2, {
-      'baseIndent': 1,
-      'switchCase': 1
-    }],
-    'space-before-function-paren': ['error', 'always']
+    indent: [2, 2, { SwitchCase: 1 }],
+    'space-before-function-paren': ['error', 'always'],
+    'react/jsx-first-prop-new-line': [1, 'always'],
+    'react/jsx-max-props-per-line': ['error', { maximum: 1 }],
+    'react/jsx-closing-bracket-location': [1, 'tag-aligned']
   },
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  'overrides': [{
-    'files': ['*.vue'],
-    'rules': {
-      'indent': 'off'
-    }
-  }]
+  parser: 'babel-eslint'
 }
